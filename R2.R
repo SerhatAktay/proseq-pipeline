d494765ef320fcc8c323ea5db36b560310216a8c
Serhat Aktay <61043167+SerhatAktay@users.noreply.github.com>
2023-06-01 11:51:01 +0200

    Update R2.R
    Reverted to original file

--- R2.R
+++ R2.R
@@ -10,22 +10,22 @@ refGene = read.table(paste0(output_folder, "/refGene_allTranscripts_withHeader.t
 
 refGeneAct = subset(refGene, geneName %in% Active[,4])
 
-write.table(refGeneAct[,c("chr","PPs","PPe","geneName","geneName","strand")], file=paste0(output_folder, "/ppPolII.txt"), col.names=F, row.names=F, quote=F, sep="\t")
-write.table(refGeneAct[,c("chr","DIVs","DIVe","geneName","geneName","strand")], file=paste0(output_folder, "/divTx.txt"), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(refGeneAct[,c("chr","PPs","PPe","geneName","strand")], file=paste0(output_folder, "/ppPolII.txt"), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(refGeneAct[,c("chr","DIVs","DIVe","geneName","strand")], file=paste0(output_folder, "/divTx.txt"), col.names=F, row.names=F, quote=F, sep="\t")
 
 shortGenes = subset(refGeneAct, txEnd-txStart<=750)
 refGeneAct_ = subset(refGeneAct, txEnd-txStart>750)
 shortGenes$geneLength <- shortGenes[,3] - shortGenes[,2]
 
-write.table(shortGenes[,c("chr","txStart","txEnd","geneName","geneName","strand")], file=paste0(output_folder, "/shortGenes.txt"), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(shortGenes[,c("chr","txStart","txEnd","geneName","strand")], file=paste0(output_folder, "/shortGenes.txt"), col.names=F, row.names=F, quote=F, sep="\t")
 write.table(shortGenes[,c("chr","txStart","txEnd","strand","geneName","TSS","CPS","DIVs","DIVe","PPs","PPe","GBs","GBe","CPSs","CPSe","TWs","TWe","promC1","promC2")], 
 						file=paste0(output_folder, "/shortGenesAllInfo.txt"), col.names=T, row.names=F, quote=F, sep="\t")
 
-write.table(refGeneAct_[,c("chr","CPSs","CPSe","geneName","geneName","strand")], file=paste0(output_folder, "/CPS.txt"), col.names=F, row.names=F, quote=F, sep="\t")
-write.table(refGeneAct_[,c("chr","TWs","TWe","geneName","geneName","strand")], file=paste0(output_folder, "/TW.txt"), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(refGeneAct_[,c("chr","CPSs","CPSe","geneName","strand")], file=paste0(output_folder, "/CPS.txt"), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(refGeneAct_[,c("chr","TWs","TWe","geneName","strand")], file=paste0(output_folder, "/TW.txt"), col.names=F, row.names=F, quote=F, sep="\t")
 
 refGeneAct_ = subset(refGeneAct_,GBe-GBs>1)  #ensuring no negative gene body lengths remain.
-write.table(refGeneAct_[,c("chr","GBs","GBe","geneName","geneName","strand")], file=paste0(output_folder, "/geneBody.txt"), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(refGeneAct_[,c("chr","GBs","GBe","geneName","strand")], file=paste0(output_folder, "/geneBody.txt"), col.names=F, row.names=F, quote=F, sep="\t")
 
 save.image()
 q()
